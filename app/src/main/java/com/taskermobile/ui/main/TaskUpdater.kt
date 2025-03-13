03a43238e516d53bc61f0e73f38b6a4cb73d2ccb
KolfinnaK <kolfinnakolbeinsdottir@gmail.com>
2025-03-13 13:24:10 +0000

    Delete taskupdater

--- app/src/main/java/com/taskermobile/ui/main/TaskUpdater.kt
+++
@@ -1,7 +0,0 @@
-package com.taskermobile.ui.main
-
-import com.taskermobile.data.model.Task
-
-interface TaskUpdater {
-    fun updateTaskInDatabaseAndBackend(task: Task)
-}
--- app/src/main/java/com/taskermobile/ui/viewmodels/TaskUpdater.kt
+++
@@ -1,7 +0,0 @@
-package com.taskermobile.ui.viewmodels
-
-import com.taskermobile.data.model.Task
-
-interface TaskUpdater {
-    fun updateTaskInDatabaseAndBackend(task: Task)
-}
